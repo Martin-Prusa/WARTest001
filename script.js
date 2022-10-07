ed1e4debb52d75b4b36f6fe158e257bd3b5b8c83
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-10-07 08:44:55 +0200

    f

---
+++ script.js
@@ -0,0 +1,35 @@
+//------------------------------1-------------------------------------
+function getProgress(status) {
+    if(status >= 100) return `Už jen chvilku! ${status}%`
+    if(status >= 75) return `Aplikace vykresluje vaše data. ${status}%`
+    if(status >= 50) return `Přihlašujeme vás. ${status}%`
+    else return `Hledáme váš účet ${status}%`
+}
+
+console.log(getProgress(13))
+console.log(getProgress(80))
+
+//---------------------------2----------------------------------------
+function compact(arr) {
+    return arr.filter(i => i)
+}
+
+console.log(compact([1, 0, 2, 5, true]))
+console.log(compact(["React"]))
+
+//---------------------------3----------------------------------------
+function distributeGold(gold) {
+    const arr = [0, 0]
+    let user = 0
+    while (gold.length !== 0) {
+        if(gold[0] >= gold[gold.length-1]) {
+            arr[user] += gold.shift()
+        } else {
+            arr[user] += gold.pop()
+        }
+        user = user === 0 ? 1 : 0
+    }
+    return arr
+}
+
+console.log(distributeGold([2,3,8,9]))
